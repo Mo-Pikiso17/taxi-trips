bde1299928e282f0ed419d17b275acdde429dfe4
Moddy Pikiso <moddypikiso17@gmail.com>
2021-11-23 16:50:24 +0200

    try and fix the find trip by region function

--- taxi-trips.js
+++ taxi-trips.js
@@ -52,19 +52,30 @@ module.exports = function TaxiTrips(pool) {
         
     }
 
-    async function findIncomeByRegNumber(route, regNumber) {
-        
-        var taxi =  await pool.query('SELECT reg_number FROM taxi WHERE reg_number_id = $1', [regNumber]);
-        var fare =  await pool.query('SELECT taxi_fare FROM route WHERE taxi_fare = $1', [route]);
+    async function findIncomeByRegNumber(regNumber) {
+        // get taxi id
+        // use it when getting route_id
+        // use route_id to get taxi fare from routes table
+
+        var taxi =  await pool.query('SELECT id FROM taxi WHERE reg_number= $1', [regNumber]);
+        var taxiId = taxi.rows[0].id
+
+        var route =  await pool.query('SELECT route_id FROM trips WHERE taxi_id= $1', [taxiId]);
+        var routeId = route.rows[0].route_id
+
+
+        var routeFare =  await pool.query('SELECT sum(taxi_fare) as tIncome FROM routes WHERE id = $1', [routeId]);
+        return routeFare.rows[0].tincome;
 
     }
 
     async function findTotalIncomePerTaxi(params) {
-        
+    // might need to use inner join to extract data from trips and routes??
     }
 
     async function findTotalIncome(params) {
-        
+    // might need to use inner join to extract data from trips and routes??
+
     }
 
     return{
